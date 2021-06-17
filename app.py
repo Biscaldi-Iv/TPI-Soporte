788ed10d34935f155d10ce45267db597d2f5018a
Ivan Biscaldi <ibiscaldi@frro.utn.edu.ar>
2021-06-17 20:32:55 -0300

    registro simplificado - ok

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for, request
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.utils import redirect
 """
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
@@ -22,15 +23,30 @@ class Users(db.Model):
     time_regist = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
-        return "User: {} {} {}".format(self.nickname, self.fullname, self.time_regist)
+        return "User: nick={} name={} time={}".format(self.nickname, self.fullname, self.time_regist)
 
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
-        pass
+        nkn = request.form['Nick']
+        # sin verificar contra
+        ps = request.form['Password']
+        fn = request.form['full']
+        em = request.form['mail']
+        new_user: Users = Users(
+            nickname=nkn, pasword=ps, fullname=fn, email=em)
+
+        try:
+            db.session.add(new_user)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return new_user
+
     else:
-        return render_template('index.html')
+        usuarios = Users.query.all()
+        return render_template('index.html', usuarios=usuarios)
 
 
 if __name__ == "__main__":
