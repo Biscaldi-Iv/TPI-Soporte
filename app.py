487b18796d2b199d0d775da1043cabca055e657f
Ivan Biscaldi <ibiscaldi@frro.utn.edu.ar>
2021-06-17 18:35:03 -0300

    semi base

--- app.py
+++ app.py
@@ -1,10 +1,36 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
+from flask_sqlalchemy import SQLAlchemy
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Sequence
+from sqlalchemy.orm import sessionmaker, relationship
+"""
+from datetime import datetime
+
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
 
-@app.route('/')
+
+class Users(db.Model):
+    nickname = db.Column(db.String(50), primary_key=True)
+    pasword = db.Column(db.String(16), nullable=False)
+    fullname = db.Column(db.String(50))
+    email = db.Column(db.String(50))
+    time_regist = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return "User: {} {} {}".format(self.nickname, self.fullname, self.time_regist)
+
+
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        pass
+    else:
+        return render_template('index.html')
 
 
 if __name__ == "__main__":
