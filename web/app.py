360b56307289a5550129f64247f1a74b73612111
Matias <santolarimatias@gmail.com>
2021-07-02 16:30:24 -0300

    Actualizacion de templates y static

--- web/app.py
+++ web/app.py
@@ -4,9 +4,6 @@ from flask_sqlalchemy import SQLAlchemy
 from werkzeug.utils import redirect
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tester.db'
-db = SQLAlchemy(app)
-
 
 @app.route('/', methods=['POST', 'GET'])
 def home():
@@ -14,7 +11,7 @@ def home():
     if request.method == 'POST':
         pass
     else:
-        return render_template('home_ext.html')
+        return render_template('logIn.html')
 
 
 @app.route('/register', methods=['POST', 'GET'])
@@ -22,31 +19,7 @@ def register():
     if request.method == 'POST':
         pass
     else:
-        return render_template('register_ext.html')
-
-
-@app.route('/users_control', methods=['POST', 'GET'])
-def usr_control():
-    if request.method == 'POST':
-        """"
-        nkn = request.form['Nick']
-        # sin verificar contra
-        ps = request.form['Password']
-        fn = request.form['full']
-        em = request.form['mail']
-        new_user: Users = Users(
-            nickname=nkn, pasword=ps, fullname=fn, email=em)
-
-        try:
-            db.session.add(new_user)
-            db.session.commit()
-            return redirect('/')
-        except Exception:
-            return Exception
-            """
-    else:
-        usuarios = Users.query.all()
-        return render_template('usr_control_ext.html')
+        return render_template('signUp.html')
 
 
 if __name__ == '__main__':
