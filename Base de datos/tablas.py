cbe7d522a7fefea5b95bde44abedd22eacd8e14d
Matias <santolarimatias@gmail.com>
2021-09-10 17:10:03 -0300

    tabla usuario v2

--- Base de datos/tablas.py
+++ Base de datos/tablas.py
@@ -2,13 +2,30 @@
 import pymysql
 from connection import DataBase
 
+class Users():
+    def __init__(self, username: str, nombre: str, apellido: str, password: str, email: str) -> None:
+        self.username = username
+        self.nombre = nombre
+        self.apellido = apellido
+        self.password = password
+        self.email = email
+
+    def __repr__(self):
+        return f"Users({self.username},{self.nombre},{self.apellido},{self.password},{self.email})"
+
 class UsuarioData(DataBase):
     def GetOne(self,username):
-        self.cursor.execute("select username,nombre,apellido from usuario where username=?",(username,))
-        return self.cursor.fetchAll()
+        self.cursor.execute("select username,nombre,apellido,password,email from usuario where username=%s",(username,))
+        return self.cursor.fetchone()
+
+    def GetAll(self):
+        self.cursor.execute("select * from usuario",)
+        return self.cursor.fetchall()
 
 
 
 
 usuario=UsuarioData()
 print(usuario.GetOne("pepito"))
+u=Users(*usuario.GetOne("pepito").values())
+print(u)
