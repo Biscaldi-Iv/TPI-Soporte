a577579efffe059d461ccc49ac4442c15d5c8be5
Biscaldi-Iv <ivanbisc12@gmail.com>
2021-10-25 17:48:55 -0300

    captcha registro

--- mvc/controllers/routes.py
+++ mvc/controllers/routes.py
@@ -1,11 +1,22 @@
 import datetime
 
-from  flask import request, url_for, Blueprint, session, render_template, redirect
+from  flask import request, url_for, Blueprint, session, render_template, redirect, json
+import requests
 from entities.models import Users
 from bussiness.usuarios_logic import UserLogic
 from typing import Dict
 
 global_scope= Blueprint('justintime', __name__, template_folder='templates')
+
+def is_human(captcha_response):
+    """ Validating recaptcha response from google server
+        Returns True captcha test passed for submitted form else returns False.
+    """
+    secret = "6Lf1OfMcAAAAAB4GZ2FfuMoGxla1-mKBO9plTYQ1"
+    payload = {'response':captcha_response, 'secret':secret}
+    response = requests.post("https://www.google.com/recaptcha/api/siteverify", payload)
+    response_text = json.loads(response.text)
+    return response_text['success']
 
 @global_scope.before_request
 def SessionCheck():
@@ -76,21 +87,24 @@ def register():
     if request.method == 'POST':
         """la unica forma de llegar a register con POST es registrando una cuenta-
         esta pagina registrara la cuenta y enviara a home"""
-        context=dict()
+        context={'captcha':'6Lf1OfMcAAAAAP2rSnX8Q4D35Lv6f1R0gC_JrEZ-'}
         user_logic=UserLogic()
         newuser=Users(request.form['username'], request.form['name'],
                   request.form['lastname'], request.form['password'],
                   request.form['email'])
-
+        captcha_response = request.form['g-recaptcha-response']
         if user_logic.get(newuser.username) is None:
             """registra"""
             #paso 1- chequear si las contraseñas coinciden
             if newuser.check_password(request.form['passconfirm']):
-                user_logic.register(newuser)
-                print("usuario registrado")
-                session['user']=newuser.username
-                session['auth']=1
-                return redirect('/home')
+                if is_human(captcha_response):
+                    user_logic.register(newuser)
+                    session['user']=newuser.username
+                    session['auth']=1
+                    return redirect('/home')
+                else:
+                    context.update({'error':'No paso recaptcha', 'lastdata':newuser})
+                    return render_template('register/register.html', **context)
             else:
                 """no coinciden las contras"""
                 context.update({'error':'Las contraseñas deben coincidir',
@@ -98,7 +112,7 @@ def register():
         else:
             """usuario ya registrado"""
             context.update({'error': 'Nombre de Usuario ya registrado',
-                            'lastdata': newuser})
+                            'lastdata': newuser,})
         """tendra el dict con el error surgido y recupera los datos para el form"""
         return render_template('register/register.html', **context)
 
@@ -107,7 +121,7 @@ def register():
     else:
         """method=get"""
         u=Users('','','','','')
-        context={'error':'', 'lastdata':u}
+        context={'error':'', 'lastdata':u, 'captcha':'6Lf1OfMcAAAAAP2rSnX8Q4D35Lv6f1R0gC_JrEZ-'}
         return render_template('register/register.html',**context)
 
 @global_scope.route('/logout', methods=['POST','GET'])
